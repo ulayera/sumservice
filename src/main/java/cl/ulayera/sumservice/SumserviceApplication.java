e31b32840353e9fc3fb491e64c23ec5440780332
Ulises Layera <ulayera@gmail.com>
2020-11-24 21:41:14 -0300

    commit inicial

---
+++ src/main/java/cl/ulayera/sumservice/SumserviceApplication.java
@@ -0,0 +1,13 @@
+package cl.ulayera.sumservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/ulayera/sumservice/SumserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ulayera.sumservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
